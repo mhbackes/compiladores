d02640148478c1bd455abb8720bd277607fa3030
prlanzarin <prlanzarin@inf.ufrgs.br>
2016-04-15 12:04:32 -0300

    fun + funheader + listOfPar, attr_array, out + listOfOut, return, while, funcall

--- etapa3/ast.h
+++ etapa3/ast.h
@@ -19,6 +19,7 @@
     AST(AST_VARDEC)     \
     AST(AST_ARRDEC)     \
     AST(AST_FUNDEC)     \
+    AST(AST_FUNHD)      \
     AST(AST_FUNCALL)    \
     AST(AST_ARRACESS)   \
     AST(AST_LCMD)		\
